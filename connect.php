27f61ecb782b94efae52667b4cb7b4f803db1fb6
dounia-elg <dounia.elg20@gmail.com>
2024-12-17 11:17:01 +0100

    Connecter Mysql avec Php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "127.0.0.1";
+$username = "root"; 
+$password = "password"; 
+ 
+$database = "blog";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: {$conn->connect_error}");
+}
+?>
